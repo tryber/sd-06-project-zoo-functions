b363749b43bea376665c975f1e673eb3d8e95278
ricknois <rickssti@outlook.com>
2020-08-21 20:07:13 -0300

    6° requisito

--- src/zoo.js
+++ src/zoo.js
@@ -43,8 +43,8 @@ function isManager(id) {
   return employees.some(emp => emp.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
