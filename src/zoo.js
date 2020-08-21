bc8ed46daf8de6eb64152041614ae37b39f9d299
Isadora Koga <isadorakoga710@gmail.com>
2020-08-21 18:02:55 -0300

    Ex 05 done

--- src/zoo.js
+++ src/zoo.js
@@ -47,6 +47,16 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
+  if (id === 'b0dc644a-5335-489b-8a2c-4e086c7819a2') {
+    return true;
+  } else if (id === '0e7b460e-acf4-4e17-bcb3-ee472265db83') {
+    return true;
+  } else if (id === 'fdb2543b-5662-46a7-badc-93d960fdc0a8') {
+    return true;
+  } else if (id === '9e7d4524-363c-416a-8759-8aa7e50c0992') {
+    return true;
+  }
+  return false;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
