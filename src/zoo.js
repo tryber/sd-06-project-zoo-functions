04c11c6c13d1aa5b5c6f209ae1e7b47cabd824e3
Daniele Perse <danieleperse3@gmail.com>
2020-08-21 19:49:40 -0300

    requisito 1 retorna somente vazio - preciso melhorar o codigo para passar no requsiito

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-/*-
+/*
 eslint no-unused-vars: [
   "error",
   {
@@ -10,10 +10,17 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const {animals} = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  const listOfAnimals = animals.filter(animal => {
+    if (animal.id === ids && ids.length > 0) {
+      return animal;
+    }
+  });
+  return listOfAnimals;
 }
+
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
