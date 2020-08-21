b5a65d59f27efbf58a7f6bf4463b2eb6739895d5
DehSartoreto <fixsartoreto@gmail.com>
2020-08-21 18:35:58 -0300

    codeclimate again

--- src/zoo.js
+++ src/zoo.js
@@ -57,15 +57,14 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   return employees;
 }
 
-function animalCount(species = "") {
+function animalCount(species = '') {
   // seu código aqui
   const allCounts = {};
-  if (species !== "") {
-    return animals.filter(pet => pet.name === species)[0].residents.length;
-  } else {
-    animals.forEach(pet => allCounts[pet.name] = pet.residents.length);
-    return allCounts;
+  if (species !== '') {
+    return animals.filter(pet => {pet.name === species[0].residents.length});
   }
+  animals.forEach(pet => allCounts[pet.name] = pet.residents.length);
+  return allCounts;
 }
 console.log(animalCount());
 
