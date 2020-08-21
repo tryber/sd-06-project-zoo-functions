cdc061ce05ac9a5c75d8cbaae5ed679fc48d551d
Leandro Parisi <lepcbelisario@gmail.com>
2020-08-21 17:46:14 -0300

    Requisito 5 - corrigindo CC 2

--- src/zoo.js
+++ src/zoo.js
@@ -105,15 +105,33 @@ function isManager(id) {
   const managersIds = employees.flatMap(element => element.managers);
   if (managersIds.includes(id)) {
     return true;
-  } 
-    return false;
+  }
+  return false;
 }
 
 console.log(isManager());
+console.log('-----6------');
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  }
+  return newEmployee
 }
+
+console.log(addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
+      [
+        '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
+        'a67a36ee-3765-4c74-8e0f-13f881f6588a',
+      ],
+      [
+        'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
+        '210fcd23-aa7b-4975-91b7-0230ebb27b99',
+      ]));
 
 function animalCount(species) {
   // seu código aqui
