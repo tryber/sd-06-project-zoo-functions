30d063faf6674105727dd722bb62b37608759ed7
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-21 19:16:31 -0300

    02- requisito2

--- src/zoo.js
+++ src/zoo.js
@@ -14,15 +14,19 @@ const { animals } = require('./data');
 
 function animalsByIds(...ids) {
   const arrayResult = [];// array para receber animais advindos do rest
-  ids.forEach(id => arrayResult
+  ids.forEach(id => arrayResult // array push tds animais filtrados de id igual retornados no filter
   .push(...animals
   .filter(animal => animal.id === id)));
   return arrayResult;
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-}
+function animalsOlderThan(animal, age) { // array com tds animals
+  const animalCollectionType = animals.filter(animalType => animalType.name === animal);
+  return animalCollectionType.every(animalCollectionResident => animalCollectionResident.residents
+  .every(animalResidents => animalResidents.age > age));
+}// s/ este every ele ñ percorre
+console.log(animalsOlderThan('otters', 7));
+console.log(animalsOlderThan('penguins', 10));
 
 function employeeByName(employeeName) {
   // seu código aqui
