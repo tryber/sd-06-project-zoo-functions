d1c87326d595c31999e9a4324667539a96779463
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-08-21 17:10:43 -0300

    Subindo função addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -25,7 +25,6 @@ console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-
 }
 
 function employeeByName(employeeName) {
@@ -44,6 +43,9 @@ function isManager(id) {
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  const newWorker = createEmployee({id, firstName, lastName}, {managers, responsibleFor});
+  return employees.push(newWorker);
+
 }
 
 function animalCount(species) {
