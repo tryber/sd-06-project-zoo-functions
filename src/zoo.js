f09eb4079b72c715993909107719d50cf6b9b8fa
jederson <jmacedo.ti@gmail.com>
2020-08-21 16:51:54 -0300

    Requeriment 02

--- src/zoo.js
+++ src/zoo.js
@@ -18,8 +18,12 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  //Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada
+  const {residents: selectedsAnimal} = data.animals.find(item => item.name === animal);
+  //console.log(selectedsAnimal);
+  return selectedsAnimal.every(animal => animal.age >= age);
 }
-
+animalsOlderThan('lions', 6);
 function employeeByName(employeeName) {
   // seu código aqui
 }
