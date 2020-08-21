7edfa736575d01adac9015b827e57d031002c8dd
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-21 14:45:00 -0300

    Projeto Zoo Functions do Tio RamireZ \o/

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(ids) {
-  // seu código aqui
+  // seu código aquiiii
 }
 
 function animalsOlderThan(animal, age) {
