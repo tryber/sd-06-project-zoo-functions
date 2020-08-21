67b12be01d8fe4d970a47b8310991605c5985663
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-08-21 20:21:31 -0300

    fix code climate

--- src/zoo.js
+++ src/zoo.js
@@ -51,7 +51,10 @@ const addEmployee = () => {};
 const animalCount = (species) => {
   const animalCounts = {};
   if (!species) {
-    animals.forEach(({ name, residents }) => animalCounts[name] = residents.length);
+    animals.forEach(({ name, residents }) => {
+      animalCounts[name] = residents.length;
+      return 0;
+    });
   } else {
     return animals.find(({ name }) => name === species).residents.length;
   }
@@ -63,7 +66,7 @@ const entryCalculator = (entrants) => {
     return 0;
   }
   const { Adult = 0, Child = 0, Senior = 0 } = entrants;
-  return `${Adult * 49.99 + Child * 20.99 + Senior * 24.99}`;
+  return `${(Adult * 49.99) + (Child * 20.99) + (Senior * 24.99)}`;
 };
 
 function animalMap(options) {
