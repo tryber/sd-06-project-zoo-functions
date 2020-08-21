05590d3ac387de2801e1006208a008b0f93c01e4
FlavioSugano <flavio.sugano@gmail.com>
2020-08-21 18:05:54 -0300

    Requisito 1: adequações ao code climate

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  return data.animals.filter(animal => {
+  return data.animals.filter((animal) => {
     for (let i = 0; i < ids.length; i += 1) {
       if (animal.id === ids[i]) {
         return true;
