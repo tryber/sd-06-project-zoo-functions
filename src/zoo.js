25438219558dfe1933656b8bd1afa4921ef08929
Leandro Parisi <lepcbelisario@gmail.com>
2020-08-21 17:13:08 -0300

    Requisito 4 - crrigindo CC

--- src/zoo.js
+++ src/zoo.js
@@ -83,13 +83,13 @@ const personalInfo = {
 const associatedWith = {
   managers: [
     'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+    '9e7d4524-363c-416a-8759-8aa7e50c0992',
   ],
   responsibleFor: [
     '0938aa23-f153-4937-9f88-4858b24d6bce',
     '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
-  ]
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+  ],
 };
 
 function createEmployee(personal, associated) {
