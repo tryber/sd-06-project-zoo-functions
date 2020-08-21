ca8f94b5f454b4f6824bf22aec4c7570028cfabf
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-08-21 18:30:59 -0300

    Requisito 5 - isManager

--- src/zoo.js
+++ src/zoo.js
@@ -40,8 +40,12 @@ function createEmployee({ id, firstName, lastName }, { managers, responsibleFor 
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.map(element => element.managers)
+    .map(item => item.includes(id))
+    .some(finder => finder === true);
 }
+
+// console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
