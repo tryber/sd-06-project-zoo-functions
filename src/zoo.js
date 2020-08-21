10f9a44924a3980b2b064cb92e1f062b83933269
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-08-21 17:34:09 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/zoo.js
+++ src/zoo.js
@@ -10,9 +10,10 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { animals } = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return animals.filter(animal => ids.some(id => id === animal.id));
 }
 
 function animalsOlderThan(animal, age) {
