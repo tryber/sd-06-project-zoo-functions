d9a3ad14b73c166b5f4ab04221099fb9c596985c
Isadora Koga <isadorakoga710@gmail.com>
2020-08-21 16:34:56 -0300

    CC arrumado

--- src/zoo.js
+++ src/zoo.js
@@ -21,7 +21,7 @@ function animalsByIds(...ids) {
   } else if (ids.length === 1) {
     return animals.filter(animal => ids.includes(animal.id));
   }
-  return animals.filter(animal => ids.includes(animal.id))
+  return animals.filter(animal => ids.includes(animal.id));
 }
 
 console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
