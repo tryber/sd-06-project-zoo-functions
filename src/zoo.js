5387df653819a3a151b125a477c4a902216d1aaa
Paulo Frazão <paulo4187@gmail.com>
2020-08-21 19:40:33 -0300

    requisito animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -10,10 +10,16 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { animals } = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqu
+function animalsByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  } 
+return animals.filter((animal) => ids.includes(animal.id));
+
 }
+
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
