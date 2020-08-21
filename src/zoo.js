e713da9acb19f46f6e7f10f6b9e6495949ead8be
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-08-21 16:06:41 -0300

    1rst requirement

--- src/zoo.js
+++ src/zoo.js
@@ -10,9 +10,15 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+const { animals } = require('./data');
 
-function animalsByIds(ids) {
-  // seu código test
+function animalsByIds(...ids) {
+  if (!ids) {
+    return [];
+  }
+  else {
+    return animals.filter((animal) => (ids.includes(animal.id)));
+  }
 }
 
 function animalsOlderThan(animal, age) {
