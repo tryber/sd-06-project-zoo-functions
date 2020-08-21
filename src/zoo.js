9bb117fe8b4fb5d28585f8074e1eaf78a5cee2ea
Marxneves <marxkhristie@gmail.com>
2020-08-21 16:24:04 -0300

    Requisito 4 realizado

--- src/zoo.js
+++ src/zoo.js
@@ -36,7 +36,15 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
 }
 
 function isManager(id) {
