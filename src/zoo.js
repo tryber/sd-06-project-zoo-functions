cab334fadef3a0a07243e985dbd15719562917d6
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-08-21 18:33:07 -0300

    issues req 4

--- src/zoo.js
+++ src/zoo.js
@@ -27,7 +27,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return newColaborator = Object.assign({}, personalInfo, associatedWith);
+  return Object.assign({}, personalInfo, associatedWith);
 }
 
 function isManager(id) {
