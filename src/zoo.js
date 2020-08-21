58c49a4115fd7dc792cfcf7816438149cfdb3f5f
kissyla <kissylamottapereira@gmail.com>
2020-08-21 19:38:25 -0300

    req 1: função animalsById - de novo e de novo

--- src/zoo.js
+++ src/zoo.js
@@ -28,7 +28,7 @@ function employeeByName(employeeName) {
     return [];
   }
   return data.employees
-  .filter((eName) => eName === firstName || eName === lastName);
+  .filter((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
