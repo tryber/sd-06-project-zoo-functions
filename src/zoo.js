efc1be40ca9ce63613d8ba06d5ec62320fcff96b
Paulo Afonso Volpin <paulo.volpin@gmail.com>
2020-08-21 16:07:56 -0300

    Task1: implementation of function animalsById

--- src/zoo.js
+++ src/zoo.js
@@ -13,7 +13,18 @@ const data = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui 
-  //Starting
+  //verifyAnimalGroup: dado um id a função percorre todo o array data.animals passando por...
+  //cada grupo de animal e verificando se o grupo possui o id passado, se der match...
+  //esse grupo é retornados dentro do acumulador groupsWithId. OBS: se mais de um grupo de ...
+  //animais possuir um mesmo id, ambos os grupos serão retornados nessa implementação.
+  const verifyAnimalGroup = (receivedId) => data.animals.reduce( (groupsWithId, animalGroup) => (animalGroup.id === receivedId) ? animalGroup : groupsWithId);
+  
+  //Se soubessemos que há somente um id para procuramos, a função acima bastaria para fazer isso...
+  //mas como não sabemos implementamos um reduce para percorrer todo o input id, pois pode ser...
+  //um único valor ou um array de ids.
+  //E para cumprir o requisito de retornar um array vazio caso a função seja chamada sem passar...
+  //nenhum valor, iniciamos o acumulador animalsGroupsThatMatchIds como um array vazio.
+  return ids.reduce( (animalsGroupsThatMatchIds, id) => animalsGroupsThatMatchIds.concat(verifyAnimalGroup(id)), []);
 }
 
 function animalsOlderThan(animal, age) {
