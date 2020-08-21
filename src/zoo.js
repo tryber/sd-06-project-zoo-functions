55732ba8eec9abaac64ed716188ad9a368960f00
isabellavjs <isabellavjs@gmail.com>
2020-08-21 16:30:34 -0300

    Requirements 2 - animalsOlderThan

--- src/zoo.js
+++ src/zoo.js
@@ -11,23 +11,26 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 const { animals } = require('./data');
+// const { animals } = require('./data');
 
 function animalsByIds(...ids) {
   if (!ids) return [];
   return ids.map(id => data.animals.find(animal => animal.id === id));
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
+function animalsOlderThan(animal, age) { 
+  return data.animals.find(getAnimal => getAnimal.name === animal) 
+  .residents.every((ageAnimal) => ageAnimal.age > age);
 }
+console.log(animalsOlderThan('otters', 7));
 
 function employeeByName(employeeName) {
   if (!employeeName) return {};
   return data.employees.find((name) => {
-    if (name.firstName === `${employeeName}` || name.lastName === `${employeeName}`) {return name};
+    if (name.firstName === `${ employeeName }` || name.lastName === `${employeeName}`) {return name}
+    return name;
   });
 }
-console.log(employeeByName('Nigel'));
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
