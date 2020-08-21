98aa4ccb3658ce687b795666d4373c6c56bac05b
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-08-21 15:34:20 -0300

    more than halfway done

--- src/zoo.js
+++ src/zoo.js
@@ -12,39 +12,90 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  if (!ids[0]) {
-    return [];
-  }
-
   return data.animals.filter(animal => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return (
+    data
+      .animals
+      .find(anim => anim.name === animal)
+      .residents
+      .every(resident => resident.age >= age)
+  )
 }
 
-function employeeByName(employeeName) {
-  // seu código aqui
+function employeeByName(employeeName = {}) {
+  return (
+    data
+      .employees
+      .find(emp => emp.firstName === employeeName || emp.lastName === employeeName)
+  )
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const { firstName, lastName, id } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+
+  return {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  }
 }
 
 function isManager(id) {
-  // seu código aqui
+  return (
+    data
+      .employees
+      .some(employee => employee.managers.includes(id))
+  )
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  }
+
+  data.employees = [...data.employees, newEmployee];
 }
 
 function animalCount(species) {
-  // seu código aqui
+  if (species) {
+    return (
+      data
+        .animals
+        .find(animal => animal.name === species)
+        .residents
+        .length
+    )
+  }
+
+  return (
+    data
+      .animals
+      .reduce((start, next) => {
+        const specie = next.name;
+        const count = next.residents.length;
+        start[specie] = count
+        return start
+      }, {})
+  )
 }
 
-function entryCalculator(entrants) {
-  // seu código aqui
+function entryCalculator(entrants = 0) {
+  const possible = ['Adult', 'Senior', 'Child'];
+
+  return (
+    possible
+      .reduce((start, next) => start + data.prices[next] * (entrants[next] || 0), 0)
+  )
 }
 
 function animalMap(options) {
