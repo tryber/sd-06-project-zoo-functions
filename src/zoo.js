6a5b8f31910dd06f7bbf72080c0908ea8359f75f
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-08-21 16:54:04 -0300

    requisito 2 funçao animalsOlderThan correta

--- src/zoo.js
+++ src/zoo.js
@@ -21,10 +21,12 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  return data.animals.find(obgAnimal => animal === obgAnimal.name)
+    .residents.every(animalResident =>  animalResident.age >= age);
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  // seu código aqui  
 }
 
 function createEmployee(personalInfo, associatedWith) {
