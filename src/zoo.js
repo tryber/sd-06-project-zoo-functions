199c1fe456db95915d68ce62618617220b23092e
William <williamjog@hotmail.com>
2020-08-21 17:57:29 -0300

    1st requirement

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,22 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+// 1- Implemente a função animalsByIds:
+// Caso receba nenhum parâmetro, necessário retornar um array vazio
+// Ao receber como parâmetro um único id, retorna os animais com este id
+// Ao receber mais de um id, retorna os animais que têm um desses ids
 
-function animalsByIds(ids) {
-  // seu código aqui
+const data = require('./data');
+const { animals } = require('./data');
+
+function animalsByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  const filteredAnimals = animals.filter(animal => ids.includes(animal.id));
+  return filteredAnimals;
 }
+
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
