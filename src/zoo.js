8fab2a9078a95e9219afa2bed1f0adcf6a979202
CristianoNunes <cristianonunes99@gmail.com>
2020-08-21 18:22:28 -0300

    Requisito 1 - Fixbugs COdeClimate

--- src/zoo.js
+++ src/zoo.js
@@ -11,7 +11,7 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(...params) {  
+function animalsByIds(...params) {
   return data.animals.filter(animal => params.includes(animal.id));
 }
 
