a16f7967074d837955a12b15c0d7e01a5ab769c1
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-08-22 10:34:48 -0300

    Resolução do requisito 04

--- src/zoo.js
+++ src/zoo.js
@@ -18,7 +18,6 @@ const { animals, employees } = require('./data');
 // Caso receba nenhum parâmetro, necessário retornar um array vazio
 // Ao receber como parâmetro um único id, retorna os animais com este id
 // Ao receber mais de um id, retorna os animais que têm um desses ids
-
 const animalsByIds = (...ids) => animals.filter(item => ids.includes(item.id));
 // const id1 = '0938aa23-f153-4937-9f88-4858b24d6bce';
 // const id2 = 'e8481c1d-42ea-4610-8e11-1752cfc05a46';
@@ -29,7 +28,6 @@ const animalsByIds = (...ids) => animals.filter(item => ids.includes(item.id));
 // 2- Implemente a função animalsOlderThan:
 // Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie
 // possuem a idade mínima especificada
-
 const animalsOlderThan = (animal, age) => {
   const allResidents = animals
     .find(item => item.name === animal).residents;
@@ -45,22 +43,42 @@ const animalsOlderThan = (animal, age) => {
 // Sem parâmetros, retorna um objeto vazio
 // Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
 // Quando provido o último nome do funcionário, retorna o objeto do funcionário
-function employeeByName(employeeName) {
+
+const employeeByName = (employeeName) => {
   if (employeeName === undefined) {
     return {};
   }
   return employees.find(item => item.firstName === employeeName || item.lastName === employeeName);
-}
+};
 // console.log(employeeByName());
 // console.log(employeeByName('Emery'));
 // console.log(employeeByName('Wishart'));
 
 
 // ====================================
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// 4- Implemente a função createEmployee:
+// Cria um novo colaborador a partir de objetos contendo informações pessoais e
+// gerentes e animais gerenciados.
+const createEmployee = (personalInfo, associatedWith) => {
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  // console.log(managers);
+  // console.log(responsibleFor);
+  return { id, firstName, lastName, managers, responsibleFor };
+};
+const personalInfo = { id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe' };
+const associatedWith = {
+  managers: ['c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'],
+  responsibleFor: ['0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'] };
+console.log(createEmployee(personalInfo, associatedWith));
 
+
+// ====================================
 function isManager(id) {
   // seu código aqui
 }
