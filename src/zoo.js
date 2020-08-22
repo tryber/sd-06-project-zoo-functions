c8a4e61c466f8ce4508eb37ad8795b6f296976b6
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-08-22 10:48:10 -0300

    Resolução do requisito 05

--- src/zoo.js
+++ src/zoo.js
@@ -62,26 +62,31 @@ const employeeByName = (employeeName) => {
 const createEmployee = (personalInfo, associatedWith) => {
   const { id, firstName, lastName } = personalInfo;
   const { managers, responsibleFor } = associatedWith;
-  // console.log(managers);
-  // console.log(responsibleFor);
+
   return { id, firstName, lastName, managers, responsibleFor };
 };
-const personalInfo = { id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe' };
-const associatedWith = {
-  managers: ['c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992'],
-  responsibleFor: ['0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'] };
-console.log(createEmployee(personalInfo, associatedWith));
+// const personalInfo = { id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+//   firstName: 'John',
+//   lastName: 'Doe' };
+// const associatedWith = {
+//   managers: ['c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+//     '9e7d4524-363c-416a-8759-8aa7e50c0992'],
+//   responsibleFor: ['0938aa23-f153-4937-9f88-4858b24d6bce',
+//     '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+//     'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'] };
+// console.log(createEmployee(personalInfo, associatedWith));
 
 
 // ====================================
-function isManager(id) {
-  // seu código aqui
-}
+// 5- Implemente a função isManager:
+// Testa se o id passado é de um gerente
+const isManager = (id) => {
+  const allManagers = employees.flatMap(item => item.managers);
+  return allManagers.includes(id);
+};
+// console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+// console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
