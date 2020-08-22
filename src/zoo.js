c9d053c0638d5299da6002c80c2294a21d5b918c
Vagner Pereira <sousa.vagner82@gmail.com>
2020-08-22 16:16:47 -0300

    Requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -43,7 +43,7 @@ function isManager(id) {
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
-  return employees.push({id, firstName, lastName, managers, responsibleFor});
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
