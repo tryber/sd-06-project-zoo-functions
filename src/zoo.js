ec6baa39f40417e0ff6a4006e236acd73c402b2f
Paulo <paulocheloni@gmail.com>
2020-08-22 15:04:48 -0300

    corrigindo lint

--- src/zoo.js
+++ src/zoo.js
@@ -69,7 +69,8 @@ function animalCount(species = animals.map(animal => animal.name)) {
   const animalsCounted = {};
   animals.filter(animal => species.some(eachAnimalName => eachAnimalName === animal.name))
   .forEach(function (animalName) {
-    animalsCounted[animalName.name] = animalName.residents.length; });
+    animalsCounted[animalName.name] = animalName.residents.length;
+  });
   return animalsCounted;
 }
 
