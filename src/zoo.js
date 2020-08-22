a309a0936ec188aaaf9247c0f92252b5d376a3bd
Bruna Franciele <brunafranciele1902@gmail.com>
2020-08-22 09:42:48 -0300

    consertando codeclimate - requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -94,7 +94,7 @@ function schedule(dayName) {
         obj.Monday = 'CLOSED';
         return obj;
       }
-      obj[dia[0]] = `Open from ${ dia[1].open }am until ${ dia[1].close -12 }pm`;
+      return (obj[dia[0]] = `Open from ${dia[1].open}am until ${dia[1].close -12}pm`);
     });
     return obj;
   }
