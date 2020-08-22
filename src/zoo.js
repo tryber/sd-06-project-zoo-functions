23cbb3c58bcfe2a180b6aabd7bfe11688e415fce
Flavio Oliveira <flavsoliver@gmail.com>
2020-08-22 13:30:10 -0300

    enviando requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -14,9 +14,9 @@ const data = require('./data');
 const { animals } = require('./data');
 
 /*  1- Implemente a função animalsByIds:
-Caso receba nenhum parâmetro, necessário retornar um array vazio
-Ao receber como parâmetro um único id, retorna os animais com este id
-Ao receber mais de um id, retorna os animais que têm um desses ids */
+ Caso receba nenhum parâmetro, necessário retornar um array vazio
+ Ao receber como parâmetro um único id, retorna os animais com este id
+ Ao receber mais de um id, retorna os animais que têm um desses ids */
 
 function animalsByIds(...ids) {
   const arrayAnimals = [];
@@ -31,9 +31,20 @@ console.log(animalsByIds());
 console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 
+/*  2- Implemente a função animalsOlderThan:
+ Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta
+ espécie possuem a idade mínima especificada*/
+
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  const array = animals
+  .find(findAnimal => findAnimal.name === animal);
+  const result = array.residents
+  .every(element => element.age > age);
+  return result;
 }
+
+console.log(animalsOlderThan('otters', 7));
+
 
 function employeeByName(employeeName) {
   // seu código aqui
