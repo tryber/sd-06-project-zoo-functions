2324d1784569e2ab0f43f4dbce2db322d3e4fbeb
Paulo <paulocheloni@gmail.com>
2020-08-22 15:17:20 -0300

    corrigindo lint

--- src/zoo.js
+++ src/zoo.js
@@ -13,7 +13,7 @@ const data = require('./data');
 const { animals, employees, prices } = require('./data');
 
 function animalsByIds(...ids) {
-  if (typeof ids !== null) {
+  if (typeof ids) {
     return data.animals
       .filter(element => element.id === ids[0])
       .concat(data.animals.filter(element => element.id === ids[1]));
