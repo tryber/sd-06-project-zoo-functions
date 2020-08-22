cb98504538795c6f688f3bb7d528486f784ae7b6
Jean Machado <jeanvictor.arq@gmail.com>
2020-08-21 22:15:03 -0300

    correcao exerc 1

--- src/zoo.js
+++ src/zoo.js
@@ -17,10 +17,7 @@ function animalsByIds(...ids) {
   return animalsCalled;
 }
 
-//console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
-
 function animalsOlderThan(animal, age) {
-  
 }
 
 function employeeByName(employeeName) {
