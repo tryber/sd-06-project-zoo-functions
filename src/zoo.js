b592b0dbf52e9ebf260262cf79f650a562873ded
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-08-22 18:29:19 -0300

    Resolução do requisito 09

--- src/zoo.js
+++ src/zoo.js
@@ -12,8 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 const { animals, employees, prices } = require('./data');
 
-
-// ====================================
+// ========================================================================================
 // 1- Implemente a função animalsByIds:
 // Caso receba nenhum parâmetro, necessário retornar um array vazio
 // Ao receber como parâmetro um único id, retorna os animais com este id
@@ -23,9 +22,9 @@ const animalsByIds = (...ids) =>
 // const id1 = '0938aa23-f153-4937-9f88-4858b24d6bce';
 // const id2 = 'e8481c1d-42ea-4610-8e11-1752cfc05a46';
 // console.log(animalsByIds(id1, id2));
+// ========================================================================================
 
-
-// ====================================
+// ========================================================================================
 // 2- Implemente a função animalsOlderThan:
 // Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie
 // possuem a idade mínima especificada
@@ -35,9 +34,9 @@ const animalsOlderThan = (animal, age) => {
 };
 // console.log(animalsOlderThan('otters', 7)); //true
 // console.log(animalsOlderThan('penguins', 10)); //false
+// ========================================================================================
 
-
-// ====================================
+// ========================================================================================
 // 3- Implemente a função employeeByName:
 // Sem parâmetros, retorna um objeto vazio
 // Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
@@ -52,9 +51,9 @@ const employeeByName = (employeeName) => {
 // console.log(employeeByName());
 // console.log(employeeByName('Emery'));
 // console.log(employeeByName('Wishart'));
+// ========================================================================================
 
-
-// ====================================
+// ========================================================================================
 // 4- Implemente a função createEmployee:
 // Cria um novo colaborador a partir de objetos contendo informações pessoais e
 // gerentes e animais gerenciados.
@@ -74,9 +73,9 @@ const createEmployee = (personalInfo, associatedWith) => {
 //     '89be95b3-47e4-4c5b-b687-1fabf2afa274',
 //     'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'] };
 // console.log(createEmployee(personalInfo, associatedWith));
+// ========================================================================================
 
-
-// ====================================
+// ========================================================================================
 // 5- Implemente a função isManager:
 // Testa se o id passado é de um gerente
 const isManager = (id) => {
@@ -87,7 +86,7 @@ const isManager = (id) => {
 // console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 
-// ====================================
+// ========================================================================================
 // 6- Implemente a função addEmployee:
 // Adiciona um funcionário no fim da lista
 const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []) => {
@@ -105,9 +104,9 @@ const addEmployee = (id, firstName, lastName, managers = [], responsibleFor = []
 //   'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
 //   '210fcd23-aa7b-4975-91b7-0230ebb27b99',
 // ]));
+// ========================================================================================
 
-
-// ====================================
+// ========================================================================================
 // 7- Implemente a função animalCount:
 // Sem parâmetros, retorna animais e suas quantidades
 // Com o nome de uma espécie de animal, retorna somente a quantidade
@@ -130,9 +129,9 @@ const animalCount = (species) => {
 // console.log(animalCount()); // Object
 // console.log(animalCount('lions')); //4
 // console.log(animalCount('snakes')); //2
+// ========================================================================================
 
-
-// ====================================
+// ========================================================================================
 // 8- Implemente a função entryCalculator:
 // Retorna 0 se nenhum argumento for passado
 // Retorna 0 se um objeto vazio for passado
@@ -157,15 +156,87 @@ const entryCalculator = (entrants) => {
 // console.log(entryCalculator({ 'Adult': 1 })); // 49.99
 // console.log(entryCalculator({ 'Child': 1 })); // 20.99
 // console.log(entryCalculator({ 'Child': 1, 'Senior': 1 })); // 45.98
+// ========================================================================================
+
+// ========================================================================================
+// 9- Implemente a função animalMap:
+// Sem parâmetros, retorna animais categorizados por localização
+// Com a opção includeNames: true especificada, retorna nomes de animais
+// Com a opção sorted: true especificada, retorna nomes de animais ordenados
+// Com a opção sex: 'female' ou sex: 'male' especificada, retorna somente nomes de animais
+//   macho/fêmea
+// Com a opção sex: 'female' ou sex: 'male' especificada e a opção sort: true especificada,
+//   retorna somente nomes de animais macho/fêmea com os nomes dos animais ordenados
+// Só retorna informações ordenadas e com sexo se a opção includeNames: true for especificada
+const animalsByLocation = (location) => {
+  const animalsLocation = {};
+  location.forEach((itemLocation) => {
+    animalsLocation[itemLocation] = animals.filter(item => item.location === itemLocation);
+    animalsLocation[itemLocation] = animalsLocation[itemLocation].map(item => item.name);
+  });
+  return animalsLocation;
+};
+
+const getResidents = (myAnimal, sex, sorted) => {
+  let residents = animals.find(item => item.name === myAnimal).residents;
+  if (sex !== 'N') {
+    residents = residents.filter(item => item.sex === sex);
+  }
+  const residentsname = residents.map(item => item.name);
+  if (sorted) {
+    residentsname.sort();
+  }
+  return residentsname;
+};
+// console.log(getResidents('lions', 'N', true));
+
+const createObjectAnimalsResidents = (itemAnimal, sex, sorted) => {
+  const myObjectAnimals = {};
+  myObjectAnimals[itemAnimal] = getResidents(itemAnimal, sex, sorted);
+  return myObjectAnimals;
+};
+// console.log(createObjectAnimalsResidents('lions'));
+
+const createArrayAnimalResidents = (myAnimals, sex, sorted) => {
+  const myArrayAnimails = [];
+  myAnimals.forEach((itemAnimal) => {
+    let animalResidents = {};
+    animalResidents = createObjectAnimalsResidents(itemAnimal, sex, sorted);
+    myArrayAnimails.push(animalResidents);
+  });
+  return myArrayAnimails;
+};
+// console.log(createArrayAnimalResidents([ 'lions', 'giraffes' ]));
+
+const animalsByLocationWithName = (location, sex, sorted) => {
+  const animalsLocation = {};
+  location.forEach((itemLocation) => {
+    let myAnimals = animals.filter(item => item.location === itemLocation);
+    myAnimals = myAnimals.map(item => item.name);
+    animalsLocation[itemLocation] = createArrayAnimalResidents(myAnimals, sex, sorted);
+  });
+  return animalsLocation;
+};
+// console.log(animalsByLocationWithName(0, [ 'NE', 'NW', 'SE', 'SW' ]));
+
+const animalMap = (options) => {
+  const location = ['NE', 'NW', 'SE', 'SW'];
+  if (options !== undefined) {
+    const { includeNames = false, sex = 'N', sorted = false } = options;
+    if (includeNames) {
+      return animalsByLocationWithName(location, sex, sorted);
+    }
+  }
+  return animalsByLocation(location);
+};
+// console.log(animalMap());
+// console.dir(animalMap({ includeNames: true}), {depth: null});
+// console.dir(animalMap({ includeNames: true, sorted: true }), {depth: null});
+// console.dir(animalMap({ includeNames: true, sex: 'female' }), {depth: null});
+// ========================================================================================
 
 
-// ====================================
-function animalMap(options) {
-  // seu código aqui
-}
-
-
-// ====================================
+// ========================================================================================
 function schedule(dayName) {
   // seu código aqui
 }
