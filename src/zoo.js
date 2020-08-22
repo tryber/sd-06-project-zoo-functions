d1a59d590d86da1c71fbf29c35cea9ca73cb6268
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-08-21 21:29:45 -0300

    issues

--- src/zoo.js
+++ src/zoo.js
@@ -48,7 +48,7 @@ function animalCount(species) {
   if (!species) {
     return Object.fromEntries(animals.map(animal => [animal.name, animal.residents.length]));
   }
-  return animals.reduce( function(acc, animal) {
+  return animals.reduce(function (acc, animal) {
     return animal.name === species ? acc + animal.residents.length : acc;
   }
   , 0);
