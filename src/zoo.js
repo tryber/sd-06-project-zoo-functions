d3c9dd720d3f12e7a8591b9d707a34c756de75ab
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-08-21 22:19:38 -0300

    Task 06 Codeclimate Fixes

--- src/zoo.js
+++ src/zoo.js
@@ -56,7 +56,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
     firstName,
     lastName,
     managers,
-    responsibleFor
+    responsibleFor,
   };
 
   employees.push(person);
