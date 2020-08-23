259bcdc9f41da9a64b7cd78d751f7dd418463c18
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-08-22 21:46:48 -0300

    Requirement 06 push 01

--- src/zoo.js
+++ src/zoo.js
@@ -47,8 +47,9 @@ function isManager(id) {
   return employees.some(employee => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = { id, firstName, lastName, managers, responsibleFor };
+  return employees.push(newEmployee);
 }
 
 function animalCount(species) {
