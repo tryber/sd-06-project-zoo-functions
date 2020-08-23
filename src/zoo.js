4a539faacb646caa68bff4c67d68a336f0261614
STEFANO-BRANZ <stefano.branz@gmail.com>
2020-08-23 12:08:17 -0300

    Requirement 07 push 01

--- src/zoo.js
+++ src/zoo.js
@@ -51,11 +51,30 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   const newEmployee = { id, firstName, lastName, managers, responsibleFor };
   return employees.push(newEmployee);
 }
-
+// On this function below i got help from Mario Duarte to understand the logic.
 function animalCount(species) {
-  // seu código aqui
+  // here i defined an constant, it's an object and will be our answer.
+  let countResult = {};
+  // The first condition is if our parameter 'species' is undefined.
+  if (!species) {
+    // We'll pass by each animal on our array of animals.
+    animals.forEach((animal) => {
+      // for each animal we'll create a constant with the name value and residents value
+      const { name, residents } = animal;
+      // then we'll add to our answer 'countResult' the 'name' as a key and the length of our
+      // residents as value of this key.
+      countResult[name] = residents.length;
+    });
+    // if we have a defined 'species' parameter like 'lions' we'll do the below.
+  } else {
+    // we'll find the animal that has the name equal to the parameter 'species', then we'll get
+    // the array of residents of this finded animal and finally gets the array length and set it as
+    // our answer 'countResult'.
+  countResult = animals.find(animal => animal.name === species).residents.length;
+  }
+  // At final we'll return our answer
+  return countResult;
 }
-
 function entryCalculator(entrants) {
   // seu código aqui
 }
