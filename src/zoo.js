324744f1dae43de175b18a381aa1c5443d8bd811
wfelipefarina <wfelipefarina@gmail.com>
2020-08-22 21:51:34 -0300

    Requisito 8, fixes codeclimate

--- src/zoo.js
+++ src/zoo.js
@@ -73,7 +73,7 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  if(!entrants || !Object.keys(entrants))  return 0;
+  if (!entrants || !Object.keys(entrants)) return 0;
 
   const { Adult = 0, Child = 0, Senior = 0 } = entrants;
   return (Adult * 49.99) + (Child * 20.99) + (Senior * 24.99);
