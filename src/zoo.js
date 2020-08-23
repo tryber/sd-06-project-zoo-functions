306c1071ea9ef5fa9b7bdf9eb47b4b3362322a32
higoramorim <1810042@aluno.univesp.br>
2020-08-23 20:20:33 -0300

    20:20 23/08

--- src/zoo.js
+++ src/zoo.js
@@ -9,102 +9,66 @@ eslint no-unused-vars: [
 ]
 */
 
-const { employees, animals, prices } = require('./data');
-/*
+const { employees, animals, prices, hours } = require('./data');
+
 function animalsByIds(...ids) {
-  if (typeof ids != null){
-    return animals
-    .filter(animal => animal.id === ids[0])
-    .concat(animals.filter(animal => animal.id === ids[1]));
-  }
-  return [];
+  return animals
+  .filter(animal => animal.id === ids[0])
+  .concat(animals.filter(animal => animal.id === ids[1]));
 }
-*/
+
 function animalsOlderThan(animalName, age) {
   return animals.every(animal => animal.name === animalName && animal.age > age);
 }
-
 
 function employeeByName(employeeName) {
   return employees
   .filter(worker => worker.firstName === employeeName ||
   worker.lastName === employeeName)[0] || {};
 }
-/*
+
 function createEmployee(personalInfo, associatedWith) {
-  return {
-    personalInfo,
-    associatedWith,
-  };
-
+  const worker = {};
+  return Object.assign(worker,personalInfo, associatedWith);
 }
-const personalInfo = employees
-.map(worker =>{
-  const id = worker.id;
-  const firstName = worker.firstName;
-  const lastName = worker.lastName;
 
-  return {
-    firstName: firstName,
-    id: id,
-    lastName: lastName,
-  };
-})
-
-const associatedWith = employees
-.map(worker =>{
-  const managers = worker.managers;
-  const responsibleFor = worker.responsibleFor;
-
-  return {
-    managers: [managers],
-    responsibleFor: [responsibleFor],
-  };
-})
-*/
 function isManager(id) {
-  return employees.some(worker => worker.managers[0] === id)
+  return employees.some(worker => worker.managers[0] === id);
 }
-/*
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  const newObject = {
-    id: id,
-    firstName: firstName,
-    lastName: lastName,
-    managers: managers,
-    responsibleFor: responsibleFor,
-  };
-  const toArray = Object.keys(newObject)
-  return Object.keys(employees).push(toArray);
+
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees.push({id, firstName, lastName, managers,responsibleFor});
 }
-*/
+
 function animalCount(species) {
   // seu código aqui
 }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+function entryCalculator(...entrants) {
+  let ticketValue = 0;
+  if(entrants.values === '' || entrants.values ==={}){
+    return 0;
+  }
+  return ticketValue;
+}  
+
 
 function animalMap(options) {
   // seu código aqui
 }
 
 function schedule(dayName) {
-  // seu código aqui
+ // seu código aqui
 }
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
 }
-/*
+
 function increasePrices(percentage) {
-  Object.entries(prices).forEach((price)=>{
-    prices[price[0]] = (price[1]*(1 + percentage/100));
-  })
+  // seu código aqui
 } 
 
-*/
 function employeeCoverage(idOrName) {
   // seu código aqui
 }
