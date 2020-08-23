12f7effd4f7ca6fda1c92e67eca329195999fd9f
higoramorim <1810042@aluno.univesp.br>
2020-08-23 16:14:35 -0300

    2 3 5

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 
 const { employees, animals, prices } = require('./data');
-
+/*
 function animalsByIds(...ids) {
   if (typeof ids != null){
     return animals
@@ -19,7 +19,7 @@ function animalsByIds(...ids) {
   }
   return [];
 }
-
+*/
 function animalsOlderThan(animalName, age) {
   return animals.every(animal => animal.name === animalName && animal.age > age);
 }
@@ -30,21 +30,56 @@ function employeeByName(employeeName) {
   .filter(worker => worker.firstName === employeeName ||
   worker.lastName === employeeName)[0] || {}
 }
-
+/*
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+  return {
+    personalInfo,
+    associatedWith,
+  };
 
+}
+const personalInfo = employees
+.map(worker =>{
+  const id = worker.id;
+  const firstName = worker.firstName;
+  const lastName = worker.lastName;
+
+  return {
+    firstName: firstName,
+    id: id,
+    lastName: lastName,
+  };
+})
+
+const associatedWith = employees
+.map(worker =>{
+  const managers = worker.managers;
+  const responsibleFor = worker.responsibleFor;
+
+  return {
+    managers: [managers],
+    responsibleFor: [responsibleFor],
+  };
+})
+*/
 function isManager(id) {
   return employees.some(worker => worker.managers[0] === id)
 }
-
+/*
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  const newObject = {
+    id: id,
+    firstName: firstName,
+    lastName: lastName,
+    managers: managers,
+    responsibleFor: responsibleFor,
+  };
+  const toArray = Object.keys(newObject)
+  return Object.keys(employees).push(toArray);
 }
-
+*/
 function animalCount(species) {
-  //seu código aqui
+  // seu código aqui
 }
 
 function entryCalculator(entrants) {
@@ -62,11 +97,13 @@ function schedule(dayName) {
 function oldestFromFirstSpecies(id) {
   // seu código aqui
 }
-
+/*
 function increasePrices(percentage) {
-  const ticketsPrices = prices.forEach((newPrice) => newPrice * (1 + percentage/100))
-  return ticketsPrices
+  Object.entries(prices).forEach((price)=>{
+    prices[price[0]] = (price[1]*(1 + percentage/100));
+  })
 } 
+*/
 function employeeCoverage(idOrName) {
   // seu código aqui
 }
