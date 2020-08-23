6426bef0182038d86bf5663b36740c781bfdcdf3
Vagner Pereira <sousa.vagner82@gmail.com>
2020-08-23 02:35:12 -0300

    Requisito 8

--- src/zoo.js
+++ src/zoo.js
@@ -62,8 +62,7 @@ function entryCalculator(entrants) {
     return 0;
   }
   const entrantKeys = Object.keys(entrants);
-  return entrantKeys.reduce((acc, current, index) => 
-  {
+  return entrantKeys.reduce((acc, current, index) => {
     const aux = prices[current] * Object.values(entrants)[index];
     return acc + aux;
   }, 0);
