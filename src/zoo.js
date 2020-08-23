684da517437b4caec5e4bcfadc1597f096f7026f
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-08-23 15:57:30 -0300

    CC

--- src/zoo.js
+++ src/zoo.js
@@ -121,7 +121,7 @@ function oldestFromFirstSpecies(id) {
   const { responsibleFor: caredAnimals } = employeeData;
   const caredAnimalsData = animalList.find(animal => animal.id === caredAnimals[0]);
   const oldestAnimalData = caredAnimalsData.residents
-    .reduce((oldest, newAnimal) => (oldest.age > newAnimal.age) ? oldest : newAnimal);
+    .reduce((oldest, newAnimal) => { return (oldest.age > newAnimal.age) ? oldest : newAnimal })
   const desiredAnimalData = Object.values(oldestAnimalData);
   return desiredAnimalData;
 }
