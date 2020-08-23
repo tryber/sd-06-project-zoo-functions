f98e9fc7b6d570433e00bfe85e6503e5b8a829e3
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-08-22 23:56:31 -0300

    issues 12

--- src/zoo.js
+++ src/zoo.js
@@ -89,7 +89,7 @@ function increasePrices(percentage) {
   const percent = percentage / 100;
   let arrayPrices = Object.entries(prices);
   Object.entries(prices).forEach(function (price, index) {
-    return arrayPrices[index][1] = Math.round((price[1] + (price[1] * percent)) * 100) / 100;
+    arrayPrices[index][1] = Math.round((price[1] + (price[1] * percent)) * 100) / 100;
   });
   arrayPrices = Object.fromEntries(arrayPrices);
   return Object.assign(prices, arrayPrices);
