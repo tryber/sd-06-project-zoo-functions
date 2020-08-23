a6d434ebc74921bd1b2b498b1ef5baaf84e55448
wfelipefarina <wfelipefarina@gmail.com>
2020-08-22 22:17:22 -0300

    Requisito 9

--- src/zoo.js
+++ src/zoo.js
@@ -84,7 +84,21 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const dates = {
+    'Tuesday': 'Open from 8am until 6pm',
+    'Wednesday': 'Open from 8am until 6pm',
+    'Thursday': 'Open from 10am until 8pm',
+    'Friday': 'Open from 10am until 8pm',
+    'Saturday': 'Open from 8am until 10pm',
+    'Sunday': 'Open from 8am until 8pm',
+    'Monday': 'CLOSED',
+  };
+
+  if (!dayName) return dates;
+
+  const day = {};
+  day[dayName] = dates[dayName];
+  return day;
 }
 
 function oldestFromFirstSpecies(id) {
