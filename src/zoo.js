63eef3b6ec2ec8751d4537c185b17ccf6b0017b9
LughWalle <civalfran1@gmail.com>
2020-08-22 19:35:52 -0500

    addEmployee check

--- src/zoo.js
+++ src/zoo.js
@@ -53,8 +53,9 @@ function isManager(id) {
   return employees.some(employee => employee.managers.some(manager => manager === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({id, firstName, lastName, managers, responsibleFor})
+  
 }
 
 function animalCount(species) {
