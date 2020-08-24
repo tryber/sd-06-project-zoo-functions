c30504689b80f0b70af7e30dc746779e2769b832
FPhilipi <fabiphilipi@gmail.com>
2020-08-24 18:59:58 -0300

    Requisito 7

--- src/zoo.js
+++ src/zoo.js
@@ -67,7 +67,14 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function animalCount(species) {
-  // seu código aqui
+  if (!species) {
+  return animals.reduce((acc, { name, residents }) =>
+    Object.assign(acc, {
+      [name]: residents.length,
+    }), {}
+  )
+}
+  return animals.find(element => element.name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
