9f273465816db27bdf879056944a90e251aca1eb
FlavioSugano <flavio.sugano@gmail.com>
2020-08-24 00:43:49 -0300

    Requisito 6: addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -49,8 +49,8 @@ function isManager(id) {
       .some(managerId => managerId === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id = '', firstName = '', lastName = '', managers = [], responsibleFor = []) {
+  data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
