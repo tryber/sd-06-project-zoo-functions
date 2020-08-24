1039519fcb5c1535e3a2b8a1b1463a46c28c64d4
Flavio Oliveira <flavsoliver@gmail.com>
2020-08-24 00:52:07 -0300

    requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -64,9 +64,33 @@ console.log(employeeByName());
 console.log(employeeByName('Emery'));
 console.log('**********Requisito 3**********');
 
+
+/* 4- Implemente a função createEmployee:
+Cria um novo colaborador a partir de objetos contendo informações pessoais e gerentes e animais gerenciados*/
+
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return { id, firstName, lastName, managers, responsibleFor };
 }
+
+console.log(createEmployee({
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+}, {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+}));
+console.log('**********Requisito 4**********');
+
 
 function isManager(id) {
   // seu código aqui
