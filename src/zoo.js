0e75a502af1a15fe81b0ca5443f7fd5a1c153559
Artur Forna <fornagieri.artur@gmail.com>
2020-08-24 08:46:32 -0300

    First commit

--- src/zoo.js
+++ src/zoo.js
@@ -8,37 +8,74 @@ eslint no-unused-vars: [
   }
 ]
 */
-
 const data = require('./data.js');
 
-function animalsByIds(id, name){
-  
+const { animals, employees } = data;
+
+function animalsByIds(...ids) {
+  const arr = [];
+  if(ids === undefined) {
+    return arr;
+  }
+  ids.map((id) => {
+    return arr.push(animals.find(animal => animal.id === id));
+  });
+  return arr;
 }
 
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'lions'));
+//Ao passar o nome de uma espécie e uma idade,
+//testa se todos os animais desta espécie possuem
+// a idade mínima especificada
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  let animalName = animals.find(animalName => animalName.name === animal);
+  let checkAge = animalName.residents.every(residentAge => age < residentAge.age);
+  return checkAge;
 }
 
+//Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário'
+//Quando provido o último nome do funcionário, retorna o objeto do funcionário'
+//Sem parâmetros, retorna um objeto vazio
 function employeeByName(employeeName) {
   // seu código aqui
+  if(employeeName == undefined) {return {}}
+  const findEmployee = employees.find( (employee) => {
+    return employee.firstName === employeeName || employee.lastName === employeeName
+  });
+  return findEmployee
 }
-
+//Cria um novo colaborador a partir de objetos contendo informações pessoais, gerentes e animais gerenciados'
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  // ??
+  return {...personalInfo,...associatedWith}
 }
 
+//Testa se o id passado é de um gerente
 function isManager(id) {
   // seu código aqui
+  const findManager = employees.some(employee => employee.managers.includes(id))
+  return findManager;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  employees.push({id, firstName, lastName, managers, responsibleFor})
 }
 
 function animalCount(species) {
   // seu código aqui
+  if(species != undefined){
+    let animalsName = animals.find( animal => animal.name == species)
+    return animalsName.residents.length
+  }else{
+    let animalCount = animals.reduce( (acc, {name, residents}) => {
+      acc[name] = residents.length
+      return acc
+    },{})
+    return animalCount
+  }
 }
 
 function entryCalculator(entrants) {
