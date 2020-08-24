6de099aa0c4393ea65bd9bfbad9a402b81cd9748
Fabio Camargos <fabiocmmcm@gmail.com>
2020-08-24 19:47:37 -0300

    eviando o 4 arrumando 3 e 1 novamente

--- src/zoo.js
+++ src/zoo.js
@@ -30,7 +30,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return Object.assign(personalInfo, associatedWith);
 }
 
 function isManager(id) {
