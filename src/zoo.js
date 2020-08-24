3896c3a8eb402a29622f1efc551229f349d11337
rodrigoderkian <derkianpromo@gmail.com>
2020-08-24 15:54:50 -0300

    Requirement 6

--- src/zoo.js
+++ src/zoo.js
@@ -35,8 +35,8 @@ function isManager(id) {
   return data.employees.some(types => types.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function animalCount(species) {
