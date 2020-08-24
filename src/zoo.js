29219eb4b6074c82d9a4d9c1fb265382a956169d
paulo-lins <paulolins100@gmail.com>
2020-08-24 16:20:10 -0300

    fix code climate

--- src/zoo.js
+++ src/zoo.js
@@ -10,35 +10,44 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-const { animals, employees } = require('./data');
+const {
+  animals,
+  employees
+} = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui..
-  return animals.filter((animal) => ids.includes(animal.id)); 
- }
-
-function animalsOlderThan(animalName, age) {
-  // seu código aqui
-  // SE AGE FOR MAIOR QUE 7, RETORNA O NOME DA ESPÉCIE SE TODOS O ANIMAIS TIVEREM MAIS DE 7 ANOS 
-  // return animals.filter(animal => animal.residents.age > age  && animal.name === animalName);percorrer o array
+  return animals.filter(animal => ids.includes(animal.id));
 }
-console.log(animalsOlderThan('lions',12))
+
+function animalsOlderThan(animal, age) {
+  return animals.find(element => element.name === animal)
+  .residents.every(element => element.age >= age);
+}
+
 function employeeByName(employeeName) {
   // seu código aqui
-}
+  // Sem parâmetros, retorna um objeto vazio
+  // Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+  // Quando provido o último nome do funcionário, retorna o objeto do funcionário
+    if (employeeName === undefined) {
+      return {}
+    } else {}
+    return employees.find(employee => employee.firstName === employeeName || employee.lastName === employeeName);
+  }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
   // 'Cria um novo colaborador a partir de objetos contendo informações pessoais, gerentes e animais gerenciados'
-  // personalInfo = employees.id
-  // SE FOR GERENTE DIZER POR QUAIS EMPREGADOS ELE É RESPONSÁVEL
+  return Object.assign({}, personalInfo, associatedWith)
 }
 
 function isManager(id) {
   // seu código aqui
-  employees.filter(idManager => idManager)
+  const man = employees.map(element => element.managers)
+  // .some(idManager => idManager === id);
 }
-
+console.log(employees.map(element => element.managers))
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
@@ -56,7 +65,21 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+//   // seu código aqui
+//   // if dayName.length === 0 {
+//   //   return {
+//       'Tuesday': 'Open from 8am until 6pm',
+//       'Wednesday': 'Open from 8am until 6pm',
+//       'Thursday': 'Open from 10am until 8pm',
+//       'Friday': 'Open from 10am until 8pm',
+//       'Saturday': 'Open from 8am until 10pm',
+//       'Sunday': 'Open from 8am until 8pm',
+//       'Monday': 'CLOSED'
+//     } else if (dayName.length !== 0) {
+//       return `${dayName}: Open from ${employees.hours[dayName].open}am to`
+//     }
+
+//   }
 }
 
 function oldestFromFirstSpecies(id) {
