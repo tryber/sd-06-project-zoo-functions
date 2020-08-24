3126b09e888fd38a2afef4df7516fc7c1a7f8fdc
rodrigoderkian <derkianpromo@gmail.com>
2020-08-24 18:52:37 -0300

    Requirement 6

--- src/zoo.js
+++ src/zoo.js
@@ -10,31 +10,43 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-
+// 1- Implemente a função animalsByIds:
+// Caso receba nenhum parâmetro, necessário retornar um array vazio
+// Ao receber como parâmetro um único id, retorna os animais com este id
+// Ao receber mais de um id, retorna os animais que têm um desses ids
 function animalsByIds(...ids) {
   return data.animals.filter(types => ids.includes(types.id));
 }
-
+// 2- Implemente a função animalsOlderThan:
+// Ao passar o nome de uma espécie e uma idade,
+// testa se todos os animais desta espécie possuem a idade mínima especificada
 function animalsOlderThan(animal, age) {
   return data.animals.find(types => types.name === animal)
   .residents.every(types => types.age >= age);
 }
-
+// 3- Implemente a função employeeByName:
+// Sem parâmetros, retorna um objeto vazio
+// Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+// Quando provido o último nome do funcionário, retorna o objeto do funcionário
 function employeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   } return data.employees.find(types => types.firstName === employeeName ||
   types.lastName === employeeName);
 }
-
+// 4- Implemente a função createEmployee:
+// Cria um novo colaborador a partir de objetos contendo
+// informações pessoais e gerentes e animais gerenciados.
 function createEmployee(personalInfo, associatedWith) {
   return Object.assign(personalInfo, associatedWith);
 }
-
+// 5- Implemente a função isManager:
+// Testa se o id passado é de um gerente
 function isManager(id) {
   return data.employees.some(types => types.managers.includes(id));
 }
-
+// 6- Implemente a função addEmployee:
+// Adiciona um funcionário no fim da lista
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   return data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
