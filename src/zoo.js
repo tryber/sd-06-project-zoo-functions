c162f824ffd9869c43e9166867aefd899567e04e
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-24 18:28:57 -0300

    8- Requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -10,26 +10,26 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-const { animals, employees, prices } = require('./data');
+const { animals, employees, prices, hours } = require('./data');
 
 function animalsByIds(...ids) {
   const arrayResult = [];// array para receber animais advindos do rest
   ids.forEach(id => arrayResult // array push tds animais filtrados de id igual retornados no filter
-  .push(...animals
-  .filter(animal => animal.id === id)));
+    .push(...animals
+      .filter(animal => animal.id === id)));
   return arrayResult;
 }
 
 function animalsOlderThan(animal, age) { // array com tds animals/animal
   const animalCollectionType = animals.filter(animalType => animalType.name === animal);
   return animalCollectionType.every(animalCollectionResident => animalCollectionResident.residents
-  .every(animalResidents => animalResidents.age > age));
+    .every(animalResidents => animalResidents.age > age));
 }// s/ este every ele ñ percorre atributo age
 
 function employeeByName(employeeName) {
   if (!employeeName) return {};// Destructuring do employeeName
   const condicao = ({ firstName, lastName }) => // qq q preencha a condição
-  (firstName === employeeName || lastName === employeeName);
+    (firstName === employeeName || lastName === employeeName);
   const result = employees.filter(condicao);
   return result[0];
 }
@@ -42,7 +42,7 @@ function createEmployee(personalInfo, associatedWith) { // assign p/ 'mesclar' a
 function isManager(id) { // 2 some intercalados 2 x percorrer
   return employees.some(element => element.managers.some(el => el === id));
 }
-                                              // default value, push manda p/ último
+// default value, push manda p/ último
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
@@ -71,7 +71,7 @@ function entryCalculator(entrants) {
   let loopBillValue = 0;            // acumulador,   valor idade/preço, index
   const totalBillValue = ageEntrants.reduce((totalBill, ageValue, i) => {
     loopBillValue = prices[ageValue] * numOfEntrants[i];
-      // cada loop pega preço tb * numero de visitantes
+    // cada loop pega preço tb * numero de visitantes
     return totalBill + loopBillValue;
   }, 0);
   return totalBillValue;
@@ -82,7 +82,22 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const dayObject = Object.keys(hours);// array dos dias
+  const visitTime = Object.values(hours);// array hs obs: acesso pelo .open/.close
+  const newObject = {};// objeto p/ salvar linhas do resultado/template string
+  dayObject.forEach((day, i) => {
+    if (day === 'Monday') {
+      newObject[day] = 'CLOSED';
+    } else {
+      newObject[day] = `Open from ${visitTime[i].open}am until ${visitTime[i].close - 12}pm`;
+    }
+  });
+  if (dayName === undefined) {
+    return newObject;
+  }
+  const line = {};
+  line[dayName] = newObject[dayName];
+  return line;
 }
 
 function oldestFromFirstSpecies(id) {
