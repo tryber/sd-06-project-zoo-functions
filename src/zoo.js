221636a02ef6967b7ecc9ac2199a12306b38f81c
Cyrano webdev <cyrano@cyranowebdev.com>
2020-08-24 20:10:02 -0300

    mais uma tentativa cclimate req9.

--- src/zoo.js
+++ src/zoo.js
@@ -132,8 +132,9 @@ function animalMap({ includeNames = false, sex = '', sorted = false } = {}) {
     locations.forEach(
       location => result[location].forEach(
         (species, index) => Object.keys(species)
-          .forEach(element => result[location][index][element].sort(),)
-      ));
+          .forEach(element => result[location][index][element].sort())
+      )
+    );
   }
   return result;
 }
