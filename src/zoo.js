6860e58c37c439d63f6e5c5610dc0d60975f0a37
Jean Machado <jeanvictor.arq@gmail.com>
2020-08-24 17:12:53 -0300

    alteracoes exerc 4

--- src/zoo.js
+++ src/zoo.js
@@ -38,8 +38,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  let addInformation;
-  addInformation =  Object.assign(personalInfo, associatedWith);
+  const addInformation =  Object.assign(personalInfo, associatedWith);
   employees.push(addInformation);
   return addInformation;
 }
