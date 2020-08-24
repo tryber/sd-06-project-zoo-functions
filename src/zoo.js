9b5a02b6f5f878067d03c9ade3c5a06d3dd3e272
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-08-24 17:22:42 -0300

    Criado objeto week na funçao schedule - Requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 const data = require('./data');
-const { animals, employees, prices } = require('./data');
+const { animals, employees, prices, hours } = require('./data');
 
 function animalsByIds(...ids) {
   if (ids === undefined) {
@@ -90,6 +90,21 @@ function animalMap(options) {
 
 function schedule(dayName) {
   // seu código aqui
+  // Sem parâmetros, retorna um cronograma legível para humanos
+  // Se um único dia for passado, retorna somente este dia em um formato legível // // para humanos
+  // ver conversão de hora PM ====> hora24 - 12
+  const week = {
+    'Tuesday': `Open from ${hours.open}am until ${hours.close - 12}pm`,
+    'Wednesday': `Open from ${hours.open}am until ${hours.close - 12}pm`,
+    'Thursday': `Open from ${hours.open}am until ${hours.close - 12}pm`,
+    'Friday': `Open from ${hours.open}am until ${hours.close - 12}pm`,
+    'Saturday': `Open from ${hours.open}am until ${hours.close - 12}pm`,
+    'Sunday': `Open from ${hours.open}am until ${hours.close - 12}pm`,
+    'Monday': `CLOSED`,
+  };
+  
+
+
 }
 
 function oldestFromFirstSpecies(id) {
