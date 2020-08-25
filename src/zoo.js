e72f66bec25c32c69f29f953ae91379581c145c7
Matheuscara <matheuscara2000@gmail.com>
2020-08-25 16:25:36 -0300

    Code-Climate

--- src/zoo.js
+++ src/zoo.js
@@ -23,7 +23,7 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   if (!employeeName) return {};
-  return data.employees.find((Element) => 
+  return data.employees.find(Element =>
     Element.firstName === employeeName || Element.lastName === employeeName);
 }
 
