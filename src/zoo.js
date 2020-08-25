abd423d60b9ea20e5e127d473e868f1edd9eb29f
LughWalle <civalfran1@gmail.com>
2020-08-24 21:54:31 -0500

    schedule CC5

--- src/zoo.js
+++ src/zoo.js
@@ -108,7 +108,7 @@ function schedule(dayName) {
   return Object.keys(hours).reduce((_, cur) => {
     return dayName === 'Monday' ?
     { [cur]: 'CLOSED' } :
-    { [dayName]: `Open from ${hours[dayName].open}am until ${hours[dayName].close - 12}pm`, }
+    { [dayName]: `Open from ${hours[dayName].open}am until ${hours[dayName].close - 12}pm` };
   });
 }
 
