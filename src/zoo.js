2dbc3a3d1508171b89958382e34165f0933931ae
ricardorosa-dev <ricardorosa@gmail.com>
2020-08-25 12:55:16 -0300

    refatorando.

--- src/zoo.js
+++ src/zoo.js
@@ -108,21 +108,36 @@ function entryCalculator(entrants) {
   return entryPrice;
 }
 
+function animalMapWithNoParams() {
+  const result = {};
+  const animalsByLocation = {};
+  const directions = ['NE', 'NW', 'SE', 'SW'];
+
+  directions.forEach(direction => {
+    const myDirection = `animals${direction}`; // animalsByLocation[myDirection] = animalsNE
+    animalsByLocation[myDirection] = animals.filter(animal => animal.location === direction);
+
+    result[direction] = animalsByLocation[myDirection].map(animal => animal.name);
+  })
+  return result;
+}
+
 function animalMap(options) {
   // seu código aqui
   const result = {};
   const animalsByLocation = {};
   const directions = ['NE', 'NW', 'SE', 'SW'];
 
-  directions.forEach((direction) => {
-    const myDirection = `animals${direction}`; // animalsByLocation[myDirection] = animalsNE
-    animalsByLocation[myDirection] = animals.filter(animal => animal.location === direction);
-    const animalNames = animalsByLocation[myDirection].map(animal => animal.name);
+  if (!options) {
+    return animalMapWithNoParams();
+  }
 
-    if (!options) {
-      result[direction] = animalsByLocation[myDirection].map(animal => animal.name);
-      return result;
-    } else if (options.includeNames) {
+  if (options.includeNames) {
+    directions.forEach((direction) => {
+      const myDirection = `animals${direction}`; // animalsByLocation[myDirection] = animalsNE
+      animalsByLocation[myDirection] = animals.filter(animal => animal.location === direction);
+      const animalNames = animalsByLocation[myDirection].map(animal => animal.name);
+
       let residents = animalsByLocation[myDirection].map(animal => animal.residents);
       if (options.sex) {
         residents = animalsByLocation[myDirection]
@@ -135,21 +150,18 @@ function animalMap(options) {
 
       result[direction] = [];
       for (let i = 0; i < animalNames.length; i += 1) {
-        if (options.sorted === true) {
-          residents[i].sort();
-        }
+        if (options.sorted === true) residents[i].sort();
         result[direction].push({ [animalNames[i]]: residents[i] });
       }
-    } else {
-      result[direction] = animalsByLocation[myDirection].map(animal => animal.name);
-    }
-    return result;
-  });
+    });
+  } else {
+    return animalMapWithNoParams();
+  }
   return result;
 }
 
 
-console.log(animalMap({ includeNames: true, sex: 'female', sorted: true }));
+console.log(animalMap());
 // { includeNames: true, sex: 'female', sorted: true }
 
 function schedule(dayName) {
