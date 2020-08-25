850315ce4c48bfb35b4acd564ddc0b97f9687994
ricardorosa-dev <ricardorosa@gmail.com>
2020-08-25 13:10:57 -0300

    refatorando.

--- src/zoo.js
+++ src/zoo.js
@@ -122,35 +122,42 @@ function animalMapWithNoParams() {
   return result;
 }
 
-function animalMap(options) {
-  // seu código aqui
+function animalMapWithIncludeNames(options) {
   const result = {};
   const animalsByLocation = {};
   const directions = ['NE', 'NW', 'SE', 'SW'];
 
+  directions.forEach((direction) => {
+    const myDirection = `animals${direction}`; // animalsByLocation[myDirection] = animalsNE
+    animalsByLocation[myDirection] = animals.filter(animal => animal.location === direction);
+    const animalNames = animalsByLocation[myDirection].map(animal => animal.name);
+
+    let residents = animalsByLocation[myDirection].map(animal => animal.residents);
+    if (options.sex) {
+      residents = animalsByLocation[myDirection]
+        .map(animal => animal.residents.filter(each => each.sex === options.sex)
+          .map(resident => resident.name));
+    } else {
+      residents = animalsByLocation[myDirection]
+        .map(animal => animal.residents.map(resident => resident.name));
+    }
+
+    result[direction] = [];
+    for (let i = 0; i < animalNames.length; i += 1) {
+      if (options.sorted === true) residents[i].sort();
+      result[direction].push({ [animalNames[i]]: residents[i] });
+    }
+  });
+  return result;
+}
+
+function animalMap(options) {
+  // seu código aqui
   if (!options) return animalMapWithNoParams();
 
-  if (options.includeNames) {
-    directions.forEach((direction) => {
-      const myDirection = `animals${direction}`; // animalsByLocation[myDirection] = animalsNE
-      animalsByLocation[myDirection] = animals.filter(animal => animal.location === direction);
-      const animalNames = animalsByLocation[myDirection].map(animal => animal.name);
+  if (options.includeNames) return animalMapWithIncludeNames(options);
 
-      let residents = animalsByLocation[myDirection].map(animal => animal.residents);
-      (options.sex) ? residents = animalsByLocation[myDirection]
-        .map(animal => animal.residents.filter(each => each.sex === options.sex)
-          .map(resident => resident.name))
-        : residents = animalsByLocation[myDirection]
-          .map(animal => animal.residents.map(resident => resident.name));
-
-      result[direction] = [];
-      for (let i = 0; i < animalNames.length; i += 1) {
-        if (options.sorted === true) residents[i].sort();
-        result[direction].push({ [animalNames[i]]: residents[i] });
-      }
-    });
-  } else return animalMapWithNoParams();
-  return result;
+  return animalMapWithNoParams();
 }
 
 
