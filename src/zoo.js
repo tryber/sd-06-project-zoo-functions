21ef1127a785353d9663d5082d12648be1266893
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-08-25 17:52:12 -0300

    Aplicado quebra de linha na função employeeByName pois o codeclimate aceita apensa 25 caracteres por linha

--- src/zoo.js
+++ src/zoo.js
@@ -22,7 +22,9 @@ function animalsOlderThan(animal, age) {
 
 function employeeByName(employeeName) {
   if (!employeeName) return {};
-  return employees.find(employee => employee.firstName === employeeName || employee.lastName === employeeName);
+  return employees
+    .find(employee => employee.firstName === employeeName || 
+      employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
