763e8e05f246062c90144c8d899925d2fd910d64
Artur Forna <fornagieri.artur@gmail.com>
2020-08-25 10:22:49 -0300

    Restam 3

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 const data = require('./data.js');
 
-const { animals, employees } = data;
+const { animals, employees, prices } = data;
 
 function animalsByIds(...ids) {
   const arr = [];
@@ -75,11 +75,33 @@ function animalCount(species) {
   return animalsCount;
 }
 
+// Retorna 0 se nenhum argumento for passado
+// Retorna 0 se um objeto vazio for passado
+// Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos
 function entryCalculator(entrants) {
   // seu código aqui
+  if (entrants === undefined || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+
+  const { Adult = 0, Child = 0, Senior = 0 } = entrants;
+  return (Adult * prices.Adult) + (Child * prices.Child) + (Senior * prices.Senior);
 }
 
-function animalMap(options) {
+// console.log(entryCalculator({ 'Adult': 2, 'Child': 3, 'Senior': 1 }));
+
+
+// Com a opção `includeNames: true` especificada, retorna nomes de animais
+// Com a opção `sorted: true` especificada, retorna nomes de animais ordenados
+// Com a opção `sex: \'female\'` ou `sex: \'male\'` especificada,
+// retorna somente nomes de animais macho/fêmea
+// Com a opção `sex: \'female\'` ou `sex: \'male\'`
+// especificada e a opção `sort: true` especificada,
+// retorna somente nomes de animais macho/fêmea com os nomes dos animais ordenados
+// Só retorna informações ordenadas e com sexo se a opção `includeNames: true` for especificada
+// Sem parâmetros, retorna animais categorizados por localização
+
+function animalMap(...options) {
   // seu código aqui
 }
 
@@ -87,13 +109,26 @@ function schedule(dayName) {
   // seu código aqui
 }
 
+// Passado o id de um funcionário,
+// encontra a primeira espécie de animal gerenciado pelo funcionário,
+// e retorna um array com nome, sexo e idade do animal mais velho dessa espécie
 function oldestFromFirstSpecies(id) {
   // seu código aqui
+  const employeeResponsibleFor = employees.find(employee => employee.id === id).responsibleFor[0];
+
+  const animalsManaged = animals.find(animal => animal.id === employeeResponsibleFor)
+  .residents.sort((a, b) => b.age - a.age)[0];
+  return [animalsManaged.name, animalsManaged.sex, animalsManaged.age];
 }
 
 function increasePrices(percentage) {
   // seu código aqui
+  Object.keys(prices).forEach((price) => {
+    prices[price] = Math.round((prices[price] * 100) * (1 + (percentage / 100))).toFixed(2) / 100;
+  });
+  return prices;
 }
+// console.log(increasePrices(50));
 
 function employeeCoverage(idOrName) {
   // seu código aqui
