38a6860843acafe8c821436b4a2352c842236057
Fabio Camargos <fabiocmmcm@gmail.com>
2020-08-25 19:21:24 -0300

    fixing cc

--- src/zoo.js
+++ src/zoo.js
@@ -82,7 +82,7 @@ function retrieveAnimals(locations, sorted, sex) {
         .map(resident => resident.name); // acessa dentro do objeto o nome do residente
         if (sorted) nameValues.sort();
         return { [nameKey]: nameValues };
-  //a chave serve para identificar qual chave seria[lion: valores]
+  // a chave serve para identificar qual chave seria[lion: valores]
       });
     animalsPerLocationWithName[location] = animals;
   });
@@ -113,7 +113,7 @@ function increasePrices(percentage) {
   prices.Adult = Math.round((prices.Adult + (prices.Adult * addDecimals)) * 100) / 100;
   prices.Senior = Math.round((prices.Senior + (prices.Senior * addDecimals)) * 100) / 100;
   prices.Child = Math.round((prices.Child + (prices.Child * addDecimals)) * 100) / 100;
-  //code Climate nao deixou colocar data.prices, entao tive que declarar no inicio do codigo
+  // code Climate nao deixou colocar data.prices, entao tive que declarar no inicio do codigo
 }
 
 function employeeCoverage(idOrName) {
