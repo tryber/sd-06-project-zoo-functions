9269023241f77fa19c1969e038de2025c0ddfad8
Jean Machado <jeanvictor.arq@gmail.com>
2020-08-24 22:45:18 -0300

    exerc 6

--- src/zoo.js
+++ src/zoo.js
@@ -51,8 +51,14 @@ function isManager(id) {
   return false;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  })
 }
 
 function animalCount(species) {
