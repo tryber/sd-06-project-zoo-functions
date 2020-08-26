09ccb56c9559badfccea5f64d64cb772fc6f95ff
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-08-25 23:45:15 -0300

    Implementado função addEmployee que adiciona um funcionário no fim da lista

--- src/zoo.js
+++ src/zoo.js
@@ -36,8 +36,13 @@ function isManager(id) {
   .some(employee => employee === id);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees
+  .push({id: id, 
+    firstName: firstName, 
+    lastName: lastName, 
+    managers: managers, 
+    responsibleFor: responsibleFor});
 }
 
 function animalCount(species) {
