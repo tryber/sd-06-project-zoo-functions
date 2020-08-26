a414af92c871c1cffcda9da399fa9bf1f9c9d9b4
Matheuscara <matheuscara2000@gmail.com>
2020-08-26 02:06:18 -0300

    Code-Climate

--- src/zoo.js
+++ src/zoo.js
@@ -63,21 +63,21 @@ function animalMap(options) {
 
 function schedule(dayName) {
   const days = Object.keys(data.hours);
-  const schedule = {}
+  const schedule = {};
 
   days.forEach((day) => {
     if (day === 'Monday') {
       schedule[day] = 'CLOSED';// 'Monday': 'CLOSED'
     } else {
       const open = data.hours[day].open;// = 8
-      const closed = data.hours[day].close -12;// = 6
-      schedule[day] = `Open from ${open}am until ${closed}pm`// 'Tuesday': 'Open from 8am until 6pm', ...
+      const closed = data.hours[day].close - 12;// = 6
+      schedule[day] = `Open from ${open}am until ${closed}pm`;// 'Tuesday': 'Open from 8am until 6pm', ...
     }
   });
 
   if (dayName === undefined) return schedule;
-  return { [dayName]: schedule[dayName] }
-} 
+  return { [dayName]: schedule[dayName] };
+}
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
