bf93280221de5282a4c3211e1933d06c9edc1510
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-26 11:06:57 -0300

    removendo comentarios

--- src/zoo.js
+++ src/zoo.js
@@ -12,58 +12,48 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  // seu código aqui
   return data.animals.filter(animal => ids.find(id => id === animal.id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
   return data.animals.find(animale => animale.name === animal)
   .residents.every(animalee => animalee.age > age);
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
   if (employeeName === undefined) return {};
   return data.employees.find(name =>
     name.firstName === employeeName || name.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
   return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
-  // seu código aqui
   return data.employees.some(idManeger => idManeger.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  // seu código aqui
   const employeeAdd = { id, firstName, lastName, managers, responsibleFor };
   data.employees.push(employeeAdd);
 }
 
 function animalCount(species) {
-  // seu código aqui
 //  if (!species)
 //  data.animals.filter(name1 => name1.name )
 //   .map(name2 => name2.popularity);
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
 //  if (!entrants) return 0;
 //  if (entrants !== {}) return 0;
 }
 
 function animalMap(options) {
-  // seu código aqui
 }
 
 function schedule(dayName) {
-  // seu código aqui
   const allDays = Object.keys(data.hours);
   const workDay = {};
 
@@ -81,15 +71,12 @@ function schedule(dayName) {
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
   // sem paramentro retorna todos os animais
   // com id retorna somente um animal
   // com nome retorna somente um animal
