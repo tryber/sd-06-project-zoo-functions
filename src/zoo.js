6a6b887b8c7f70c6ca985e6fdb5375f739fd1cfe
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-08-26 14:27:18 -0300

    Implementado a função animalCount onde, sem parâmetros, retorna animais e suas quantidades e com o nome de uma espécie de animal, retorna somente a quantidade

--- src/zoo.js
+++ src/zoo.js
@@ -42,7 +42,12 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function animalCount(species) {
-  // seu código aqui
+  if (!species) {
+    const qtdAnimals = {};
+    animals.forEach(animal => (qtdAnimals[animal.name] = animal.residents.length))
+    return qtdAnimals;
+  }
+ return animals.find(animal => animal.name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
