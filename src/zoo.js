d4b46a4ab0f09ec3e913cc39d06f598eac11fe92
Fabio Camargos <fabiocmmcm@gmail.com>
2020-08-26 15:06:29 -0300

    sera que foi 80?

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-const { prices } = require('./data');
+const { prices, hours } = require('./data');
 
 function animalsByIds(...ids) {
   if (ids === undefined) {
@@ -124,7 +124,18 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const cronogram = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED'
+  };
+  if (!dayName) return cronogram;
+  const hoursByDays = Object.keys(cronogram).find(days => days === dayName);
+  return { [hoursByDays]: cronogram[hoursByDays] };
 }
 
 function oldestFromFirstSpecies(id) {
