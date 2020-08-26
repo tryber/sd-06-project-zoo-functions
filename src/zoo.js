525075d21f954391bed4299e0783f0d2155ae637
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-25 21:40:57 -0300

    Implemente a função schedule

--- src/zoo.js
+++ src/zoo.js
@@ -10,6 +10,7 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
+// const { animals, employees } = require('./data');
 // const { employees } = require('./data');
 // const { employees } = require('./data');
 //  const { animals } = require('./data');
@@ -54,6 +55,8 @@ function animalCount(species) {
 
 function entryCalculator(entrants) {
   // seu código aqui
+//   if (!entrants) return 0;
+//   if (entrants !== {}) return 0;
 }
 
 function animalMap(options) {
@@ -62,6 +65,20 @@ function animalMap(options) {
 
 function schedule(dayName) {
   // seu código aqui
+  const allDays = Object.keys(data.hours);
+  const workDay = {};
+
+  allDays.forEach((day) => {
+    if (day === 'Monday') {
+      workDay[day] = 'CLOSED';
+    } else {
+      const openHours = data.hours[day].open;
+      const closeHours = data.hours[day].close - 12;
+      workDay[day] = `Open from ${openHours}am until ${closeHours}pm`;
+    }
+  });
+  if (dayName === undefined) return workDay;
+  return { [dayName]: workDay[dayName] };
 }
 
 function oldestFromFirstSpecies(id) {
@@ -74,6 +91,23 @@ function increasePrices(percentage) {
 
 function employeeCoverage(idOrName) {
   // seu código aqui
+  // sem paramentro retorna todos os animais
+  // com id retorna somente um animal
+  // com nome retorna somente um animal
+  // com sobrenome retorna somente um animal
+  // resolução no plantão
+  // const result = {};
+  // if (!idOrName) {
+  //   data.employees.forEach(employee => {
+  //     result [`${employee.firstName} ${employee.lastName}`] = '';
+  //     employee.responsibleFor.forEach(animalIdResponsibleFor => {
+  //       const foundAnimalName = data.animals.find(animal =>
+  //          animal.id === animalIdResponsibleFor).name;
+  //       result [`${employee.firstName} ${employee.lastName}`].push(foundAnimalName);
+  //     });
+  //   });
+  // }
+  // return result;
 }
 
 module.exports = {
