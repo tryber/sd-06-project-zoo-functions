0b4f8c8b60379169bdbac4222cf9bddf2faceee8
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-08-27 15:52:44 -0300

    declaranco prices

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 
 const data = require('./data');
-const { animals, employees } = require('./data');
+const { animals, employees, prices } = require('./data');
 
 function animalsByIds(...ids) {
   return animals.filter(animal => ids.includes(animal.id));
