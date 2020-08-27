cc1a1fce330f2d1d1ebfd388cd5edac8343a4dab
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-08-26 22:10:57 -0300

    cc2

--- src/zoo.js
+++ src/zoo.js
@@ -35,7 +35,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  return data.employees.some(gerente => gerente.managers.includes(id))
+  return data.employees.some(gerente => gerente.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
