b03e96c5e87bcc2afe6c91726cb6a0296a856560
Pedro Duarte <ikbenpedro@gmail.com>
2020-08-26 22:13:24 -0300

    increasePrices 1.0

--- src/zoo.js
+++ src/zoo.js
@@ -145,8 +145,13 @@ function oldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-
+ 
+prices.Adult =Math.round((prices.Adult*100*(1+(0.01*percentage))).toFixed(2))/100;
+prices.Senior = Math.round((prices.Senior*100*(1+(0.01*percentage))).toFixed(2))/100;
+prices.Child = Math.round((prices.Child*100*(1+(0.01*percentage))).toFixed(2))/100;
+  return prices
 }
+
 
 function employeeCoverage(idOrName) {
   // seu código aqui
