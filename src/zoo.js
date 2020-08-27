d932cc7f432749dd04e8d4d8d61c84986fa58f36
Macarena Rodriguez <macarenar697@gmail.com>
2020-08-27 15:10:29 -0300

    requisito 1, 2, 3, 4, 5, 6, 7, 8, 11, 12 e 13

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,11 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
+//  REQUISITO 1
 //  Caso receba nenhum parâmetro, necessário retornar um array vazio
 //  Ao receber como parâmetro um único id, retorna os animais com este id
 //  Ao receber mais de um id, retorna os animais que têm um desses ids
+
 function animalsByIds(...ids) {
   //  !!!!!!Reever codigo com defaulParameter!!!!!
   if (ids.length === 0) {
@@ -25,9 +27,11 @@ function animalsByIds(...ids) {
   return animalsWithId;
 }
 
+//  REQUISITO 2
 //  Ao passar o nome de uma espécie e uma idade,
 //  testa se todos os animais desta espécie possuem
 //  a idade mínima especificada
+
 function animalsOlderThan(animal, age) {
   const animalPassed = data.animals
     .find(item => item.name === animal).residents
@@ -35,9 +39,11 @@ function animalsOlderThan(animal, age) {
   return animalPassed;
 }
 
+//  REQUISITO 3
 //  Sem parâmetros, retorna um objeto vazio
 //  Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
 //  Quando provido o último nome do funcionário, retorna o objeto do funcionário
+
 function employeeByName(employeeName) {
   //  !!!!!!Reever codigo com defaulParameter!!!!!
   if (employeeName === undefined) {
@@ -48,6 +54,7 @@ function employeeByName(employeeName) {
   return employFilter;
 }
 
+//  REQUISITO 4
 //  Cria um novo colaborador a partir de objetos contendo informações
 //  pessoais e gerentes e animais gerenciados.
 //  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
@@ -57,9 +64,11 @@ function createEmployee(personalInfo, associatedWith) {
   return newEmployee;
 }
 
+//  REQUISITO 5
 //  Testa se o id passado é de um gerente
 //  Fusão de dois arrays
 //  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+
 function isManager(id) {
   const arrayManagers = data.employees.map(employ => employ.managers);
   const newArray = [];
@@ -74,15 +83,19 @@ function isManager(id) {
   return false;
 }
 
+//  REQUISITO 6
 //  https://eslint.org/docs/rules/object-shorthand
 //  Adiciona um funcionário no fim da lista
+
 function addEmployee(id = [], firstName = [], lastName = [], managers = [], responsibleFor = []) {
   const employ = { id, firstName, lastName, managers, responsibleFor };
   return data.employees.push(employ);
 }
 
+//  REQUISITO 7
 //  Sem parâmetros, retorna animais e suas quantidades
 //  Com o nome de uma espécie de animal, retorna somente a quantidade
+
 function animalCount(species) {
   if (species === undefined) {
     const quantityAnimals = {};
@@ -98,9 +111,11 @@ function animalCount(species) {
   return animalNumber.residents.length;
 }
 
+//  REQUISITO 8
 //  Retorna 0 se nenhum argumento for passado
 //  Retorna 0 se um objeto vazio for passado
 //  Retorna o preço total a ser cobrado dado o número de adultos, crianças e idosos
+
 function entryCalculator(entrants = 0) {
   const objectCount = Object.keys(entrants);
   if (entrants === 0 || objectCount.length === 0) {
@@ -117,19 +132,26 @@ function entryCalculator(entrants = 0) {
   return counter;
 }
 
+//  REQUISITO 9
+
 function animalMap(options) {
   // seu código aqui
 }
 
+//  REQUISITO 10
 //  Sem parâmetros, retorna um cronograma legível para humanos
 //  Se um único dia for passado, retorna somente este dia em um formato legível para humanos
-//  Percorrer objeto https://pt.stackoverflow.com/questions/173293/como-percorrer-um-objeto-em-javascript
+//  Percorrer objeto:
+//  https://pt.stackoverflow.com/questions/173293/como-percorrer-um-objeto-em-javascript
+
 function schedule(dayName) {
   // seu código aqui
 }
 
+//  REQUISITO 11
 //  Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo
 //  funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie
+
 function oldestFromFirstSpecies(id) {
   let animalOlder = 0;
   let showAnimal = [];
@@ -147,7 +169,9 @@ function oldestFromFirstSpecies(id) {
   return Object.values(showAnimal);
 }
 
+//  REQUISITO 12
 //  Ao passar uma porcentagem, incrementa todos os preços, arrendondados em duas casas decimais
+
 function increasePrices(percentage) {
   const newPricesObject = {};
   const personPriceArray = Object.entries(data.prices);
@@ -164,15 +188,15 @@ function increasePrices(percentage) {
   });
 }
 
+//  REQUISITO 13
 //  Sem parâmetros, retorna uma lista de funcionários e os animais pelos
 // quais eles são responsáveis Com o id de um funcionário, retorna os animais
 // pelos quais o funcionário é responsável Com o primeiro nome de um funcionário,
 // retorna os animais pelos quais o funcionário é responsável Com o último nome
 // de um funcionário, retorna os animais pelos quais o funcionário é responsável
 
-// Funcoes a executar para quando nao se pasa parametro----------------------------
-//  funcoe chamada por createEmployeeList para buscar atraves de parametro ja passado os animales
-//  por los cuales e responsable o empregado
+//  funcoe chamada por createEmployeeList(sem parametro) para buscar atraves de
+//  parametro ja passado os animales por los cuales e responsable o empregado
 function listAnimals(employ) {
   const animalsResponsibleFor = employ.responsibleFor;
   const listAnimalsDoEmploy = animalsResponsibleFor
@@ -183,8 +207,8 @@ function listAnimals(employ) {
   });
   return listAnimalsDoEmploy;
 }
-//  Pasa por cada empregado e cria objeto con nome e sobrenome, logo chama a funcoe listAnimals
-//  para tracer os animais desses empregados
+//  Pasa por cada empregado e cria objeto con nome e sobrenome, logo chama a funcoe
+//  listAnimals para tracer os animais desses empregados
 function createEmployeeList() {
   const employeObject = {};
   data.employees
@@ -195,30 +219,30 @@ function createEmployeeList() {
     });
   return employeObject;
 }
-function createObjetToShow(employe, callback) {
+//  chama a listAnimals e cria o objeto para ser retornado
+function createObjetToShow(employe) {
   const employeObject = {};
-  const animalOfEmploye = callback(employe);
+  const animalOfEmploye = listAnimals(employe);
   const nameEmploy = `${employe.firstName} ${employe.lastName}`;
   employeObject[nameEmploy] = animalOfEmploye;
   return employeObject;
 }
-
-function createEmployeeLisWithParameters(idOrName, callback) {
-  let employeObject = {};
-  data.employees.forEach((employe) => {
-    if (employe.id === idOrName || employe.firstName === idOrName) {
-      employeObject = createObjetToShow(employe, callback);
-    } else if (employe.lastName === idOrName) {
-      employeObject = createObjetToShow(employe, callback);
+//  Verificia o parametro si e firstName, id ou lastName, e enta na funcoe
+//  createObjetToShow
+function checkParameters(idOrName) {
+  let employeFound = {};
+  data.employees.forEach((empl) => {
+    if (empl.id === idOrName || empl.firstName === idOrName || empl.lastName === idOrName) {
+      employeFound = createObjetToShow(empl);
     }
   });
-  return employeObject;
+  return employeFound;
 }
 
 // Funcoe principal que deriva dependendo do parametros pasados
 function employeeCoverage(idOrName) {
   if (!idOrName) return createEmployeeList();
-  return createEmployeeLisWithParameters(idOrName, listAnimals);
+  return checkParameters(idOrName);
 }
 
 module.exports = {
