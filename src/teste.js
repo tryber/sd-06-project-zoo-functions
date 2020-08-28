bfb41c91488fd9aedd4679df5ccd88801f4ce8ca
Paulo <paulocheloni@gmail.com>
2020-08-28 12:34:04 -0300

    removendo test.js

--- src/teste.js
+++
@@ -1,9 +0,0 @@
-function set(objeto, metodo, funcao) {
-  objeto[metodo] = funcao;
-}
-const ovo = {};
-function fritar(alimento) {
-  return `${alimento} frito`;
-}
-set(ovo,'fritar',fritar);
-console.log(ovo.fritar)
