0d7baf50614056a12522071fd5c77e11b50c3031
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-08-27 21:42:27 -0300

    function animalCount() && function entryCalculator()

--- src/zoo.js
+++ src/zoo.js
@@ -43,10 +43,31 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function animalCount(species) {
+  if (species === undefined || species === '') {
+    const objVazio = {};
+    data.animals.map((animal) => { objVazio[animal.name] = animal.residents.length});
+    return objVazio;
+  } else {
+    const findTheAnimal = data.animals.find(animal => animal.name === species);
+    return findTheAnimal.residents.length;
+  }
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (!entrants || entrants === {}) {
+    return 0;
+ }
+ let totalPrice = 0;
+ if (entrants.Adult) {
+   totalPrice += (entrants.Adult * data.prices.Adult);
+ }
+ if (entrants.Senior) {
+   totalPrice += (entrants.Senior * data.prices.Senior);
+ }
+ if (entrants.Child) {
+   totalPrice += (entrants.Child * data.prices.Child);
+ }
+ return totalPrice;
 }
 
 function animalMap(options) {
