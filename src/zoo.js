8840647e2efe6ef65518ccb9ebdff4e4d4e109c3
Danilo <dansantosalfa@gmail.com>
2020-08-29 15:01:54 -0300

    Add função schedule.js

--- src/zoo.js
+++ src/zoo.js
@@ -39,6 +39,21 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
+  const allDays = Object.keys(data.hours);
+  const schedule = {};
+
+  allDays.forEach((day) => {
+    if (day === 'Monday') {
+      schedule[day] = 'CLOSED';
+    } else {
+      const openHours = data.hours[day].open;
+      const closeHours = data.hours[day].close - 12;
+      schedule[day] = `Open from ${openHours}am until ${closeHours}pm`;
+    }
+  });
+
+  if (dayName === undefined) return schedule; 
+  return { [dayName]: schedule[dayName] };
 }
 
 function oldestFromFirstSpecies(id) {
