1ffff9a9015a4774efcd1778d7077f9e6fb662dd
IzeldaValeriano <zel.valeriano@gmail.com>
2020-08-29 16:42:05 -0300

    addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -38,8 +38,8 @@ function isManager(id) {
     .some(element => element === id);
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers =[], responsibleFor = []) {
+  return employees.push({id, firstName, lastName, managers, responsibleFor});
 }
 
 function animalCount(species) {
