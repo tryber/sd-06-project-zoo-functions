1ea331635e45b85df198ec98e9dce9a6a55b5eac
vania patricia <vapatty@gmail.com>
2020-08-31 11:28:47 -0300

    Code Climate  correção

--- src/zoo.js
+++ src/zoo.js
@@ -23,90 +23,83 @@ function animalsOlderThan(animal, age) {
 }
 animalsOlderThan('lions', 4);
 function employeeByName(employeeName) {
-  // seu código aqui
+// seu código aqui
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui -Sem parâmetros, retorna um objeto vazio
+// seu código aqui -Sem parâmetros, retorna um objeto vazio
 }
 
 function isManager(id) {
-  // seu código aqui - Testa se o id passado é de um gerente
+// seu código aqui - Testa se o id passado é de um gerente
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui  - Adiciona um funcionário no fim da lista'
+// seu código aqui  - Adiciona um funcionário no fim da lista'
 }
 
 function animalCount(species) {
-  // seu código aqui - Sem parâmetros, retorna animais e suas quantidades
+// seu código aqui - Sem parâmetros, retorna animais e suas quantidades
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+// seu código aqui
 }
 
 function retrieveAnimalPerLocation(locations) {
   const animalPerLocation = {};
   locations.forEach((location) => {
-    const animal =  animals
+    const animal2 = animals
       .filter(animal => animal.location === location)
       .map(animal => animal.name);
-    if (animal.length !== 0) animalPerLocation[location] = animal;
+    if (animal2.length !== 0) animalPerLocation[location] = animal;
   });
-  return  animalPerLocation;
+  return animalPerLocation;
 }
 function retrieveAnimalByLocationWithName(locations, sorted, sex) {
-const animalByLocationWithName = {};
-locations.forEach((location) => {
-  const animal = animals
-    .filter(animal => animal.location === location)
-    .map(animal => {
-      const animalKey = animal.name;
-      const animalValue = animal.residents
-        .filter(resident => {
-          const isFilteringSex = sex !== undefined ;
-          return isFilteringSex ? resident.sex === sex : true;
-        })
-        .map(resident => resident.name);
-      if(sorted) animalValue.sort();
-      return { [animalKey]: animalValue };
-    });
-    animalByLocationWithName[location] = animal;
-});
+  const animalByLocationWithName = {};
+  locations.forEach((location) => {
+    const animal = animals
+      .filter(animal => animal.location === location)
+      .map(animal => {
+        const animalKey = animal.name;
+        const animalValue = animal.residents
+          .filter(resident => {
+            const isFilteringSex = sex !== undefined ;
+            return isFilteringSex ? resident.sex === sex : true;
+          })
+          .map(resident => resident.name);
+        if (sorted) animalValue.sort();
+        return { [animalKey]: animalValue };
+      });
+  animalByLocationWithName[location] = animal;
+  });
   return animalByLocationWithName;
 }
 
 function animalMap(options) {
-  const locations = ['NE', 'NW', 'SE', 'SW' ];
-  if (!options){return retrieveAnimalPerLocation(locations);}
+  const locations = ['NE', 'NW', 'SE', 'SW'];
+  if (!options) {return retrieveAnimalPerLocation(locations);}
 /* const includeNames = options.includeNames;
 const sorted = options.sorted; destruction em uma linha e passa por parametros na função */
   const { includeNames, sorted, sex } = options;
-  if (includeNames === undefined) {return retrieveAnimalPerLocation(locations);}
-  if (includeNames){ return retrieveAnimalByLocationWithName(locations, sorted, sex);}
+  if (includeNames === undefined) {return retrieveAnimalPerLocation(locations); }
+  if (includeNames) { return retrieveAnimalByLocationWithName(locations, sorted, sex); }
 }
 
 function schedule(dayName) {
-  // seu código aqui - Sem parâmetros, retorna um cronograma legível para humanos
+// seu código aqui - Sem parâmetros, retorna um cronograma legível para humanos
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+// seu código aqui
 }
 
 function increasePrices(percentage) {
 }
 
 function employeeCoverage(idOrName) {
-  //nomes e sobrenomes dos funcionarios sem parametros
-
-  //juntar nome e sobrenome e usar como propriedade (chave ) do objeto de retorno
-
-
-  //com o id
-  // com o nome
-  // com o sobrenome
+//nomes e sobrenomes dos funcionarios sem parametros //juntar nome e sobrenome e usar como propriedade (chave ) do objeto de retorno
 }
 
 module.exports = {
