1946c21ce0185cb0bf0b670f7f70d61b3e7d6d96
Paulo Frazão <paulo4187@gmail.com>
2020-09-01 10:53:01 -0300

    requisito createEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -44,8 +44,33 @@ function employeeByName(employeeName) {
 // console.log(employeeByName('Nigel'));
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  const newEmploye = {
+    id: id,
+    firstName: firstName,
+    lastName: lastName,
+    managers: managers,
+    responsibleFor: responsibleFor,
+  }
+  return newEmploye;
+
 }
+console.log(createEmployee({
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+}, {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+}));
 
 function isManager(id) {
   // seu código aqui
@@ -98,3 +123,13 @@ module.exports = {
   increasePrices,
   createEmployee,
 };
+
+
+const fornecedor = {
+  razaosocial: "Alura - Cursos Online",
+  cnpj: "00.000.000/0000-00",
+  email: "suporte@alura.com.br"
+};
+
+const { razaosocial: nosso } = fornecedor;
+console.log(nosso);
