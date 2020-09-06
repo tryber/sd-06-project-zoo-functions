2eae0ab7fe06ec2f25de97b196907aa83c65375c
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-09-06 15:38:21 -0300

    Implementado código da função schedule, onde sem parâmetro, retorna um cronograma com os dias e horários de funcionamento do zoológico e quando um dia específico é inserido, é retornado o dia com o hóraio de funcionamento, para isso foi criado uma nova função, calender, para criar o calendário completo quando sem parâmetro e essa função é chamada dentro da função schedule

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals, employees, prices } = require('./data');
+const { animals, employees, prices, hours } = require('./data');
 
 function animalsByIds(...ids) {
   return animals.filter(animal => ids.includes(animal.id));
@@ -61,8 +61,24 @@ function animalMap(options) {
   //
 }
 
+function calender(day, openingHours) {
+  const { open, close } = hours[day];
+  if (open === 0) {
+    openingHours[day] = 'CLOSED';   
+  } else {
+    openingHours[day] = `Open from ${open}am until ${close - 12}pm`;
+  }
+  return openingHours;
+}
+
 function schedule(dayName) {
-  //
+  openingHours = {}
+  if (!dayName) {
+    Object.keys(hours).forEach(elem => calender(elem, openingHours));
+  } else {
+    calender(dayName, openingHours);
+  }
+  return openingHours;
 }
 
 function oldestFromFirstSpecies(id) {
