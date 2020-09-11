35a2a80321723a3a4d20645f5b5e2b948ac11fa9
vapatty6848 <vapatty@gmail.com>
2020-09-11 13:14:11 -0300

    animalMap

--- src/zoo.js
+++ src/zoo.js
@@ -74,18 +74,71 @@ function entryCalculator(entrants) {
   const price = keys.reduce((cc, current) => cc + (entrants[current] * prices[current]), 0);
   return price;
 }
+
+function retrieveAnimalPerLocation(locations) {
+  const animalPerLocation = {};
+  locations.forEach((location) => {
+    const animal = data.animals
+    .filter(animalLoc => animalLoc.location === location)
+    .map(animalLoc => animalLoc.name);
+    if (animal.length !==0) animalPerLocation[location] = animal
+  });
+  return animalPerLocation
+  }
+
+function retrieveAnimalByLocationWithName(locations, sorted, sex) {
+    const animalByLocationWithName = {};
+    locations.forEach((location) => {
+      const animalBy = data.animals
+      .filter(animalBy => animalBy.location === location)
+      .map((animalBy) => {
+        const animalKey = animalBy.name;
+        const animalValue = animalBy.residents
+        .filter((resident) => {
+        const isFilteringSex = sex !== undefined;
+      return isFilteringSex ? resident.sex === sex : true;
+       })
+        .map(resident => resident.name);
+        if (sorted) animalValue.sort();
+        return { [animalKey]: animalValue };
+        });
+    animalByLocationWithName[location] = animalBy;
+    });
+    return animalByLocationWithName;
+    }
+
+function animalMap(options) {
+  const locations = ['NE', 'NW', 'SE', 'SW'];
+  if (!options) return retrieveAnimalPerLocation(locations);
+  const { includeNames, sorted, sex } = options;
+  if (includeNames === undefined) return retrieveAnimalPerLocation(locations);
+  if (includeNames) return retrieveAnimalByLocationWithName(locations, sorted, sex);
+  return retrieveAnimalByLocationWithName;
+}
+function schedule(dayName) {
+
+}
+function oldestFromFirstSpecies(id) {
+
+}
+function increasePrices(percentage) {
+
+}
+function employeeCoverage(idOrName){
+}
+
 module.exports = {
   entryCalculator,
-  // schedule,
+  schedule,
   animalCount,
-  // animalMap,
+  animalMap,
   animalsByIds,
   employeeByName,
-  // employeeCoverage,
+  employeeCoverage,
   addEmployee,
   isManager,
   animalsOlderThan,
-  // oldestFromFirstSpecies,
-  // increasePrices,
+  oldestFromFirstSpecies,
+  increasePrices,
   createEmployee,
 };
